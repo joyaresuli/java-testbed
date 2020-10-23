3d94d38d014774239300ba66a01709dd3ef66f53
joyaresuli <gioiaresuli@gmail.com>
2020-10-23 21:03:02 +0300

    Hello World v 0.2

--- src/gr/aueb/elearn/ch1/HelloWorld.java
+++ src/gr/aueb/elearn/ch1/HelloWorld.java
@@ -1,4 +1,11 @@
 package gr.aueb.elearn.ch1;
 
+/**
+ * Prints Hello World
+ * @author joya resuli
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
